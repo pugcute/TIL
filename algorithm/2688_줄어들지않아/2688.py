8d23828df63a78ba86e9663ca7f186d1a4210fca
pugcute <mjgh0135@gmail.com>
2022-03-20 01:24:22 +0900

    0320

---
+++ algorithm/2688_줄어들지않아/2688.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+for i in range(N):
+    K = int(input())
+    numbers = [[0 for _ in range(10)] for _ in range(K+1)]
+    for row in range(1, K+1):
+        numbers[row][0] = max(1, sum(numbers[row-1]))
+        for col in range(1, 10):
+            numbers[row][col] = max(1, numbers[row][col-1] - numbers[row-1][col-1])
+    print(sum(numbers[K]))
